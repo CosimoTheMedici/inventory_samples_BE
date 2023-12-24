25da679060e3846896ad1540e39f7b5a30247687
cosmas <cosmasthuku4@gmail.com>
2023-12-24 14:31:27 +0300

    Initial commit

--- models/index.js
+++ models/index.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 const uri1 = "mongodb+srv://adminhamza:adminhamza123&@cluster0.pzcviot.mongodb.net/InventoryManagementApp?retryWrites=true&w=majority";
-const uri = "mongodb://localhost:27017/InventoryManagementApp?retryWrites=true&w=majority";
+const uri8 = "mongodb://localhost:27017/InventoryManagementApp?retryWrites=true&w=majority";
+const uri = process.env.MONGODB_URI
 
 
 function main() {
